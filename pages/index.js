29c2fcd28be102e38d33f32ad04f8a18cbd50abb
sh-shafi <shafayet.hossain.dhk.bd@gmail.com>
2022-12-10 00:48:17 +0600

    Initialized!

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import Education from "../src/components/Education";
 import Head from "next/head";
-import About from "../src/components/About";
 import Contact from "../src/components/Contact";
 import Home from "../src/components/Home";
-import Publications from "../src/components/Publications";
 import Newsletter from "../src/components/Newsletter";
-import Process from "../src/components/Process";
-import NewsList from "../src/components/NewsList";
 import Layout from "../src/layout/Layout";
 
 const Index = () => {
@@ -16,12 +11,7 @@ const Index = () => {
         <title>Dizme | Home</title>
       </Head>
       <Home />
-      <About />
-      <Process />
-      <NewsList />
-      <Publications />
       <Newsletter />
-      <Education />
       <Contact />
     </Layout>
   );
